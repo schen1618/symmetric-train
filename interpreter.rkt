65228ca324df6be6604a909da1eb2bceb83dfa2a
Chris Toomey <ctt16@case.edu>
2019-03-06 18:28:34 -0600

    Update interpreter.rkt

--- interpreter.rkt
+++ interpreter.rkt
@@ -11,7 +11,7 @@
 ;; takes an expression of numbers/variables and operators and returns the value
 ;; The operators are +, -, *, /, % and division is integer division
 (define m-value
-  (lambda (exp state)
+  (lambda (exp state return break continue)
     (cond
       [(null? exp)                    (error 'undefined "undefined expression")]
       [(number? exp)                  exp]
@@ -21,18 +21,18 @@
             (equal? 'notfound
                     (get exp state))) (error 'error "variable not declared")]
       [(instate? exp state)           (get exp state)]
-      [(eq? (operator exp) '+)        (+ (m-value (left-operand exp) state)
-                                         (m-value (right-operand exp) state))]
+      [(eq? (operator exp) '+)        (+ (m-value (left-operand exp) state return break continue)
+                                         (m-value (right-operand exp) state return break continue))]
       [(and (eq? (operator exp) '-)
-            (null? (cddr exp)))       (* (m-value (left-operand exp) state) -1)]
-      [(eq? (operator exp) '-)        (- (m-value (left-operand exp) state)
-                                         (m-value (right-operand exp) state))]
-      [(eq? (operator exp) '*)        (* (m-value (left-operand exp) state)
-                                         (m-value (right-operand exp) state))]
-      [(eq? (operator exp) '/)        (quotient (m-value (left-operand exp) state)
-                                                (m-value (right-operand exp) state))]
-      [(eq? (operator exp) '%)        (remainder (m-value (left-operand exp) state)
-                                                 (m-value (right-operand exp) state))])))
+            (null? (cddr exp)))       (* (m-value (left-operand exp) state return break continue) -1)]
+      [(eq? (operator exp) '-)        (- (m-value (left-operand exp) state return break continue)
+                                         (m-value (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '*)        (* (m-value (left-operand exp) state return break continue)
+                                         (m-value (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '/)        (quotient (m-value (left-operand exp) state return break continue)
+                                                (m-value (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '%)        (remainder (m-value (left-operand exp) state return break continue)
+                                                 (m-value (right-operand exp) state return break continue))])))
 
 (define operator car)
 (define left-operand cadr)
@@ -41,7 +41,7 @@
 
 ;; adds variable/value pair to state
 (define add
-  (lambda (var value state)
+  (lambda (var value state return break continue)
     (cond
       [(instate? var state) (error 'error "variable already defined")]
       [else                 (list (cons var (add-var state)) (cons value (add-value state)))])))
@@ -51,14 +51,14 @@
 
 ;; removes variable/value pair from state, wrapper for remove-acc
 (define remove
-  (lambda (var state)
+  (lambda (var state return break continue)
     (remove-acc var state init-state)))
 
 
 ;; takes an expression and evealuates whether it is true or false
 ;; the different comparison operators are ==, !=, >, >=, <, <=, &&, ||, !
 (define m-bool
-  (lambda (exp state)
+  (lambda (exp state return break continue)
     (cond
       [(null? exp)                    (error 'undefined "undefined expression")]
       [(number? exp)                  exp]
@@ -72,36 +72,36 @@
       [(and (not (list? exp))
             (equal? 'notfound
                     (get exp state))) (error 'error "variable not declared")]
-      [(eq? (operator exp) '==)       (equal? (m-eval (left-operand exp) state)
-                                              (m-eval (right-operand exp) state))]
-      [(eq? (operator exp) '!=)       (not (equal? (m-eval (left-operand exp) state)
-                                                   (m-eval (right-operand exp) state)))]
-      [(eq? (operator exp) '>)        (> (m-eval (left-operand exp) state)
-                                         (m-eval (right-operand exp) state))]
-      [(eq? (operator exp) '>=)       (>= (m-eval (left-operand exp) state)
-                                          (m-eval (right-operand exp) state))]
-      [(eq? (operator exp) '<)        (< (m-eval (left-operand exp) state)
-                                         (m-eval (right-operand exp) state))]
-      [(eq? (operator exp) '<=)       (<= (m-eval (left-operand exp) state)
-                                          (m-eval (right-operand exp) state))]
-      [(eq? (operator exp) '&&)       (and (m-eval (left-operand exp) state)
-                                           (m-eval (right-operand exp) state))]
-      [(eq? (operator exp) '||)       (or (m-eval (left-operand exp) state)
-                                          (m-eval (right-operand exp) state))]
-      [(eq? (operator exp) '!)        (not (m-eval (left-operand exp) state))]
+      [(eq? (operator exp) '==)       (equal? (m-eval (left-operand exp) state return break continue)
+                                              (m-eval (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '!=)       (not (equal? (m-eval (left-operand exp) state return break continue)
+                                                   (m-eval (right-operand exp) state return break continue)))]
+      [(eq? (operator exp) '>)        (> (m-eval (left-operand exp) state return break continue)
+                                         (m-eval (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '>=)       (>= (m-eval (left-operand exp) state return break continue)
+                                          (m-eval (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '<)        (< (m-eval (left-operand exp) state return break continue)
+                                         (m-eval (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '<=)       (<= (m-eval (left-operand exp) state return break continue)
+                                          (m-eval (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '&&)       (and (m-eval (left-operand exp) state return break continue)
+                                           (m-eval (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '||)       (or (m-eval (left-operand exp) state return break continue)
+                                          (m-eval (right-operand exp) state return break continue))]
+      [(eq? (operator exp) '!)        (not (m-eval (left-operand exp) state return break continue))]
       [else                           'invalid_expression])))
 
 
 ;; declares a variable
 (define m-declare
-  (lambda (statement state)
+  (lambda (statement state return break continue)
     (cond
       [(null? statement)           (error 'error "undefined expression")]
-      [(null? (dec-exp statement)) (add (dec-var statement) 'novalue state)]
+      [(null? (dec-exp statement)) (add (dec-var statement) 'novalue state return break continue)]
       [(list? (dec-exp statement)) (add (dec-var statement)
-                                        (m-eval (dec-value statement) state)
-                                        state)]
-      [else                        (add (dec-var statement) (dec-value statement) state)])))
+                                        (m-eval (dec-value statement) state return break continue)
+                                        state return break continue)]
+      [else                        (add (dec-var statement) (dec-value statement) state return break continue)])))
 
 (define dec-exp cddr)
 (define dec-var cadr)
@@ -109,14 +109,14 @@
 
 ;; assigns a value to a variable
 (define m-assign
-  (lambda (statement state)
+  (lambda (statement state return break continue)
     (cond
       [(null? statement)            (error 'error "undefined expression")]
       [(instate? (ass-var statement)
                  state)             (m-declare (list (equal-sign statement)
                                                      (ass-var statement)
-                                                     (m-eval (ass-value statement) state))
-                                               (remove (ass-var statement) state))]
+                                                     (m-eval (ass-value statement) state return break continue))
+                                               (remove (ass-var statement) state return break continue) return break continue)]
       [else                         (error 'error "variable not declared")])))
 
 (define equal-sign car)
@@ -125,25 +125,23 @@
   
 ;; returns an evaluated expression
 (define m-return
-  (lambda (statement state)
+  (lambda (statement state return break continue)
     (cond
       [(null? statement)                           (error 'error "undefined expression")]
-      [(eq? #t (m-eval (ret-exp statement) state)) (add 'return 'true state)]
-      [(eq? #f (m-eval (ret-exp statement) state)) (add 'return 'false state)]
-      [else                                        (add 'return
-                                                        (m-eval (ret-exp statement) state)
-                                                        state)])))
+      [(eq? #t (m-eval (ret-exp statement) state)) (return 'true)]
+      [(eq? #f (m-eval (ret-exp statement) state)) (return 'false)]
+      [else                                        (return (m-eval (ret-exp statement) state return break continue))])))
 
 (define ret-exp cadr)
 
 ;; if statement that returns the evaluated first statement if true or the second statement if false,
 ;; if it exists
 (define m-if
-  (lambda (statement state)
+  (lambda (statement state return break continue)
     (cond
       [(null? statement)                           (error 'error "undefined statement")]
-      [(eq? #t (m-bool (if-cond statement) state)) (m-state (then-statement statement) state)]
-      [(not (null? (else statement)))              (m-state (else-statement statement) state)]
+      [(eq? #t (m-bool (if-cond statement) state)) (m-state (then-statement statement) state return break continue)]
+      [(not (null? (else statement)))              (m-state (else-statement statement) state return break continue)]
       [else                                        state])))
 
 (define if-cond cadr)
@@ -152,51 +150,60 @@
 (define else-statement cadddr)
 
 ;; while loop that evaluates the statement until the condition is no longer true
-(define m-while
-  (lambda (statement state)
+(define m-while-helper
+  (lambda (statement state return break continue)
     (cond
       [(null? statement)              (error 'error "undefined statement")]
       [(eq? #t (m-bool
                 (while-cond statement)
-                state))               (m-while statement (m-state (while-statement statement) state))]
+                state))               (m-while statement (m-state (while-statement statement) state return break continue) return break continue)]
       [else                           state])))
+
+(define m-while
+  (lambda (statement state)
+    (call/cc
+     (lambda (break)
+       (m-while-helper statement state return break continue)))))
 
 (define while-cond cadr)
 (define while-statement caddr)
 
 ;; evaluates the block of statements between the brackets
 (define block
-  (lambda (statement-list state return)
+  (lambda (statement-list state return break continue)
     (cond
-      ((null? statement-list) (state))
-      (else (block (cdr statement-list) (deleteList (m-state (car statement-list) (addList(state)))) (lambda (v) (return (m-eval v state))))))))
+      ((null? statement-list) (return state))
+      (else (deleteList (state-list (cdr statement-list) (addList(state))) return break continue)))))
 
 ;; m-state calls the appropriate function to evaluate the statement
 (define m-state
-  (lambda (statement state)
+  (lambda (statement state return break continue)
     (cond
-      [(eq? (identifier statement) 'var)    (m-declare statement state)]
-      [(eq? (identifier statement) '=)      (m-assign statement state)]
-      [(eq? (identifier statement) 'return) (m-return statement state)]
-      [(eq? (identifier statement) 'if)     (m-if statement state)]
-      [(eq? (identifier statement) 'while)  (m-while statement state)]
+      [(eq? (identifier statement) 'var)    (m-declare statement state return break continue)]
+      [(eq? (identifier statement) '=)      (m-assign statement state return break continue)]
+      [(eq? (identifier statement) 'return) (m-return statement state return break continue)]
+      [(eq? (identifier statement) 'break)  (break state)]
+      [(eq? (identifier statement) 'if)     (m-if statement state return break continue)]
+      [(eq? (identifier statement) 'while)  (m-while statement state return break continue)]
       [else                                 (error 'error "undefined expression")])))
 
 (define identifier car)
 
-;; interpret-tree takes a list of statements (lists) as an argument
+;; state-list takes a list of statements (lists) as an argument
 ;; and returns the evaluated collection of statements
-(define interpret-tree
-  (lambda (tree state)
+(define state-list
+  (lambda (tree state return break continue)
     (cond
       [(null? tree) (get 'return state)]
-      [(pair? tree) (interpret-tree (cdr tree) (m-state (car tree) state))])))
+      [(pair? tree) (state-list (cdr tree) (m-state (car tree)) (lambda (v) v))])))
       
 
-;; interpret takes a filename and runs it through interpret-tree
+;; interpret takes a filename and runs it through state-list
 (define interpret
   (lambda (filename)
-    (interpret-tree (parser filename) init-state)))
+    (call/cc
+     (lambda (return)
+    (state-list (parser filename) init-state return break continue)))
 
 
 ;;;; ******************************************************************************************
@@ -245,24 +252,21 @@
 
 ;; evaluates an expression
 (define m-eval
-  (lambda (exp state)
+  (lambda (exp state return break continue)
     (cond
       [(equal? 'invalid_expression
-               (m-bool exp state)) (m-value exp state)]
-      [else                        (m-bool exp state)])))     
+               (m-bool exp state return break continue)) (m-value exp state return break continue)]
+      [else                        (m-bool exp state return break continue)])))     
 
 ;; deletes the top layer of the input state
 (define deleteList
   (lambda (state)
-    (cond
-      ((null? state) '())
-      ((list? (car state)) (append (car state) (cdr state)))
-      (else (cons (car state) (cdr state))))))
+    ((cdr state))))
 
 
-;; adds a layer to the input state
+;; adds athe top layer of the input state
 (define addList
   (lambda (state)
-    (list state)))
-
-(define x 'cmon)
+    (cond
+      ((null? (cdr state)) (cons (car state)))
+      (else (cons (cadr state) state)))))
